e9609e7db9ffb326585cd1bcbc4e07650b032de5
nicoWeb31 <nico.riot@free.fr>
2020-12-12 12:41:39 +0100

    presentation

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -4,6 +4,9 @@ const Test = () => {
     return (
         <div>
             <h1>Aller il me faut maquette idee pour le disign du site !!! Merci</h1>
+            <h3>Wiwi: ux ui charte graphique</h3>
+            <h3>gasp : redaction text, presentation ex...</h3>
+            <h3>plop : retour sur le fonctionnement, test </h3>
         </div>
     )
 }
