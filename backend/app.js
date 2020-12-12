a3a7e32b86ee934c07ea4ba1e285e9eb7557c8c7
nicoWeb31 <nico.riot@free.fr>
2020-12-12 12:14:12 +0100

    test deployement

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
 const app = express();
 import morgan from 'morgan';
 
@@ -7,8 +8,18 @@ if(process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'));
 }
 
-
-
+//for production
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.get('*',(req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+    })
+}else{
+    app.get('/', (req,res)=>{
+        res.send('API is running!!!')
+    })
+}
 
 
 
