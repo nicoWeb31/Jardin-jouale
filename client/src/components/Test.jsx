cc144060e3ec2764b3209402ab10f93b51473027
nicoWeb31 <nico.riot@free.fr>
2020-12-12 12:02:00 +0100

    test deployement

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Test = () => {
     return (
         <div>
-            <h1>Aller il me faut maquette idee pour le disign du site !!!</h1>
+            <h1>Aller il me faut maquette idee pour le disign du site !!! Merci</h1>
         </div>
     )
 }
