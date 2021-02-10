24b98b6d167b68af49ff690ea9f309c0eddebae6
nicoWeb31 <nico.riot@free.fr>
2021-02-10 16:08:18 +0100

    login back

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,44 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import { catchAsync } from '../utils/catchAsync.js';
+import AppError from '../utils/appError.js'
 
 export const singnup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    
+    // const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SERCRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
 
     res.status(201).json({
         status: 'success',
-        user: newUser,
+        token,
+        data: {
+            user: newUser,
+        },
     });
 });
+
+
+export const login = (req, res,next) => {
+
+    const { email, password } = req.body;
+
+    //check if email and password exist
+    if(!email || !password) {
+    next(new AppError('Please provide email and password',400));
+    }
+
+    const token = '';
+    res.status(200).json({
+        status: 'success',
+        token,
+    });
+
+}
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { singnup } from '../controllers/authController.js';
+import { singnup,login } from '../controllers/authController.js';
 
 const router = express.Router();
 
 
 router.post('/singnup', singnup);
+router.post('/login', login);
+
 
 
 
