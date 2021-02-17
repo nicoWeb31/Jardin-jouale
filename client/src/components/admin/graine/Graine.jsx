8fdd7a2b06ff5b1018494b3303096ab0e2552197
nicoWeb31 <nico.riot@free.fr>
2021-02-17 22:09:14 +0100

    seed table ok

--- client/src/components/admin/graine/Graine.jsx
+++ client/src/components/admin/graine/Graine.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from 'react';
+import './graine.style.scss';
 
 const Graine = () => {
-    return (
-        <div>
-            Liste des graines
-        </div>
-    )
-}
+    const listeSeeds = [];
 
-export default Graine
+    return (
+        <div className="containerAdminSeed">
+            <h1>Liste des graines</h1>
+            <div>
+                <i className="fas fa-plus-circle fa-5x addBtn"></i>
+            </div>
+            <table class="responstable">
+                <tr>
+                    <th>Legume</th>
+                    <th>cultivar</th>
+                    <th>Début-semis</th>
+                    <th>Fin-SEmis</th>
+                    <th>Quantités</th>
+                    <th>Commentaire</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>Tomate</td>
+                    <td>Green-zebra</td>
+                    <td>12-03</td>
+                    <td>14-05</td>
+                    <td>300</td>
+                    <td>Super génial</td>
+                    <td>
+                        <div className="btnContent">
+                            <i class="fas fa-edit fa-2x btnEdit"></i>
+                            <i class="fas fa-trash fa-2x btnTrash"></i>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Tomate</td>
+                    <td>Green-zebra</td>
+                    <td>12-03</td>
+                    <td>14-05</td>
+                    <td>300</td>
+                    <td>Super génial</td>
+                    <td>
+                        <div className="btnContent">
+                            <i class="fas fa-edit fa-2x btnEdit"></i>
+                            <i class="fas fa-trash fa-2x btnTrash"></i>
+                        </div>
+                    </td>
+                </tr>
+                {/* {listeSeeds.map((seed)=>{
+                    return (
+                <tr>
+                    <td>Tomate</td>
+                    <td>Green-zebra</td>
+                    <td>12-03</td>
+                    <td>14-05</td>
+                    <td>300</td>
+                    <td>Super génial</td>
+
+                </tr>
+
+                        )
+                })} */}
+            </table>
+        </div>
+    );
+};
+
+export default Graine;
