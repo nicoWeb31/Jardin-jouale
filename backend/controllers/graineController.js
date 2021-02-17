aa60b4ccb3e465a5ce4baad6788b06a1546af43f
nicoWeb31 <nico.riot@free.fr>
2021-02-17 17:41:32 +0100

    add controller seed

--- backend/controllers/graineController.js
+++ backend/controllers/graineController.js
@@ -1,4 +1,18 @@
 import Semence from '../models/semenceModel.js';
-import { getAll } from './handlerFactory.js'
+import {
+    getAll,
+    createNewDoc,
+    updateOne,
+    deleteOne,
+    getOne
+} from './handlerFactory.js';
 
-export const getAllSeed =  getAll(Semence)
+export const getAllSeed = getAll(Semence);
+
+export const createSeed = createNewDoc(Semence);
+
+export const patchSeed = updateOne(Semence);
+
+export const deleteSeed = deleteOne(Semence);
+
+export const getOneSeed = getOne(Semence);
